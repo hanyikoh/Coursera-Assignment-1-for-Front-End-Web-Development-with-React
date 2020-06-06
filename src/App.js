2227b6e656cfd66244faad541a74a1929aff29e0
hanyikoh <hanyikoh@gmail.com>
2020-06-06 16:09:06 +0800

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import React, { Component } from 'react';
 //import { DISHES } from './shared/dishes'
 import './App.css'
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
