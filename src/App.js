20f9b4eb2719c141c94d43df695ff5cfb9722d35
hanyikoh <hanyikoh@gmail.com>
2020-06-06 14:33:56 +0800

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/Menucomponent'
-import { DISHES } from './shared/dishes'
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import { Navbar, NavbarBrand } from 'reactstrap';
+//import Menu from './components/MenuComponent'
+//import { DISHES } from './shared/dishes'
+import './App.css'
+import Main from './components/MainComponent';
 
-function App() {
-  const [dishes, setdishes] = useState(DISHES)
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={dishes} />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
